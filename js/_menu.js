cdbc9aa3acb790a0b435d9656af5856be83ed1e6
thomasavguld <rosen.thomas@gmail.com>
2025-05-04 21:57:59 +0200

    misc fixes in menu

--- js/_menu.js
+++ js/_menu.js
@@ -1,4 +1,4 @@
-createMenu('Inlämning', [
+createMenu('Inlämning Statistik/Dataanalys, Thomas Rosén DM24TO', [
   { name: 'Översikt', script: 'overview.js' },
   { name: 'Depression', script: 'pageOne.js' },
   { name: 'Studierelaterad stress', script: 'pageTwo.js' },
