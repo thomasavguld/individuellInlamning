f7f1407d90fbead7550a0f7ebd4dcdc73f64831b
thomasavguld <rosen.thomas@gmail.com>
2025-05-04 12:47:49 +0200

    changed menu and added a bunch of new category .js files

--- js/_menu.js
+++ js/_menu.js
@@ -1,6 +1,7 @@
 createMenu('Inlämning', [
   { name: 'Översikt', script: 'overview.js' },
-  //{ name: 'Visa ett år', script: 'one-year.js' },
-  //{ name: 'Jämför två år', script: 'compare-two-years.js' },
-  //{ name: 'Hitta trender', script: 'trends.js' }
+  //{ name: 'Sida ett', script: 'pageOne.js' },
+  //{ name: 'Sida två', script: 'pageTwo.js' },
+  //{ name: 'Slutsats', script: 'conclusion.js' },
+  //{ name: 'Källor', script: 'sources.js' },
 ]);
