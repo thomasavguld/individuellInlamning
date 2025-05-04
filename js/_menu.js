70ef1b9e834b05240e2639e88d3dafccc7c33a87
thomasavguld <rosen.thomas@gmail.com>
2025-05-04 16:11:25 +0200

    Added page three to menu

--- js/_menu.js
+++ js/_menu.js
@@ -2,6 +2,7 @@ createMenu('Inlämning', [
   { name: 'Översikt', script: 'overview.js' },
   { name: 'Sida ett', script: 'pageOne.js' },
   { name: 'Sida två', script: 'pageTwo.js' },
+  { name: 'Sida tre', script: 'pageThree.js' },
   { name: 'Slutsats', script: 'conclusion.js' },
   { name: 'Källor', script: 'sources.js' },
 ]);
