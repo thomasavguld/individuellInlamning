a097a675399f21e0de854fd1c530694cd80bc553
thomasavguld <rosen.thomas@gmail.com>
2025-05-04 12:49:14 +0200

    deleted trash code from overview.js

--- js/_menu.js
+++ js/_menu.js
@@ -1,7 +1,7 @@
 createMenu('Inlämning', [
   { name: 'Översikt', script: 'overview.js' },
-  //{ name: 'Sida ett', script: 'pageOne.js' },
-  //{ name: 'Sida två', script: 'pageTwo.js' },
-  //{ name: 'Slutsats', script: 'conclusion.js' },
-  //{ name: 'Källor', script: 'sources.js' },
+  { name: 'Sida ett', script: 'pageOne.js' },
+  { name: 'Sida två', script: 'pageTwo.js' },
+  { name: 'Slutsats', script: 'conclusion.js' },
+  { name: 'Källor', script: 'sources.js' },
 ]);
