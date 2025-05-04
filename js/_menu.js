2d643526d82f9a53c1355e93991135163ffc0090
thomasavguld <rosen.thomas@gmail.com>
2025-05-04 18:51:31 +0200

    Something with the menu  I think?

--- js/_menu.js
+++ js/_menu.js
@@ -1,8 +1,8 @@
 createMenu('Inlämning', [
   { name: 'Översikt', script: 'overview.js' },
-  { name: 'Sida ett', script: 'pageOne.js' },
-  { name: 'Sida två', script: 'pageTwo.js' },
-  { name: 'Sida tre', script: 'pageThree.js' },
+  { name: 'Depression', script: 'pageOne.js' },
+  { name: 'Studierelaterad stress', script: 'pageTwo.js' },
+  { name: 'Sömn', script: 'pageThree.js' },
   { name: 'Slutsats', script: 'conclusion.js' },
   { name: 'Källor', script: 'sources.js' },
 ]);
